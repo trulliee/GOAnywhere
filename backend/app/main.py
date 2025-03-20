818fa0ca42f8cd155bc6e34b13cee6535259b2f8
Feliz <kaungsaung2002@gmail.com>
2025-03-20 17:49:29 +0800

    Merged feliz into main and committed local changes

--- backend/app/main.py
+++ backend/app/main.py
@@ -7,11 +7,14 @@ from app.routes.prediction import router as prediction_router
 from app.routes import incidents
 from app.routes.user_incidents import router as user_incidents_router
 from app.routes.auth import router as auth_router  # Import the auth router
+from app.dashboard import router as dashboard_router  # Import the dashboard router
+from app.p2pnavigation import router as p2pnavigation_router  # Import the p2pnavigation router
+from app.notifications import router as notifications_router # Import the notifications router
 from dotenv import load_dotenv
 load_dotenv()
 
 
-# Initialize FastAPI
+# Initialize FastAPIS
 app = FastAPI()
 
 # Add CORS middleware
@@ -35,3 +38,6 @@ app.include_router(prediction_router, tags=["Prediction"])
 app.include_router(incidents.router)
 app.include_router(user_incidents_router)
 app.include_router(auth_router)  # Add the auth router
+app.include_router(p2pnavigation_router, prefix="/p2pnavigation", tags=["P2P Navigation"]) # Include the p2pnavigation router
+app.include_router(notifications_router, prefix="/notifications", tags=["Notifications"]) # Include the notifications router
+app.include_router(dashboard_router, prefix="/dashboard", tags=["Dashboard"]) # Include the dashboard router

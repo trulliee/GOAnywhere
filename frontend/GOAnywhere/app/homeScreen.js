c8f38b13b85b7be6d0f751519d557cdb6177f9c6
Bentley <78397482+TanTianGan@users.noreply.github.com>
2025-05-06 15:56:56 +0800

    added latest admin features to homescreen

--- frontend/GOAnywhere/app/homeScreen.js
+++ frontend/GOAnywhere/app/homeScreen.js
@@ -706,7 +706,18 @@ export default function HomeScreen() {
           >
             <View style={styles.menuItemRow}>
               <MaterialIcons name="people" size={24} color="#fff" style={styles.menuIcon} />
-              <Text style={styles.menuText}>User Management</Text>
+              <Text style={styles.menuText}>User Management (admin)</Text>
+            </View>
+          </TouchableOpacity>
+
+          {/* Admin Notifications Section (Admin only)*/}
+          <TouchableOpacity 
+            style={styles.menuItem} 
+            onPress={() => navigateTo('AdminNotification')}
+          >
+            <View style={styles.menuItemRow}>
+              <MaterialIcons name="admin-panel-settings" size={24} color="#fff" style={styles.menuIcon} />
+              <Text style={styles.menuText}>Admin Notifications (admin)</Text>
             </View>
           </TouchableOpacity>
 

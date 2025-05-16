df4c0e309ef34e8f1c3bb82487957309ac6097c6
sinz3715 <200000marcke@gmail.com>
2025-05-17 05:27:19 +0800

    clear up

--- frontend/GOAnywhere/app/app.js
+++ frontend/GOAnywhere/app/app.js
@@ -2,7 +2,7 @@ import React from 'react';
 import { NavigationContainer } from '@react-navigation/native';
 import { createNativeStackNavigator } from '@react-navigation/native-stack';
 
-import LoginScreen from './loginUser';
+import LoginScreen from './';
 import SignupScreen from './SignUp';
 
 const Stack = createNativeStackNavigator();
